c3f66ff1edb15d28cfb316822a682067fe6ccc2f
Andrii_Koval <andriykoval2642@gmail.com>
2021-06-21 13:42:30 +0300

    Refactor2

--- SocialTechTestTask.py
+++ SocialTechTestTask.py
@@ -56,7 +56,7 @@ def read_data_from_csv(file_name):
         it = csv.reader(f, delimiter=';')
         data = []
         for row in it:
-            if not after_test_begin(row) and desktop(row):
+            if not after_test_begin(row):
                 data.append(row)
 
     return data[1:]
@@ -80,14 +80,17 @@ def append_by_key(source, key, value):
     source[key] = v
 
 
-def get_avarage_likes_by_date(dict_to_make, source):
+def get_avarage_likes_by_date(source):
+	result_dict = {}
 	for key in source:
 		ids_temp = source.get(key)
 		unique = len(set(ids_temp))
 		if unique != 0:
-			dict_to_make[key] = len(ids_temp) / unique
+			result_dict[key] = len(ids_temp) / unique
 		else:
-			dict_to_make[key] = 0
+			result_dict[key] = 0
+
+	return result_dict
 
 
 _file_name = "AB_test_rawdata.csv"
@@ -105,26 +108,8 @@ for key in _ids_by_dates:
         else:
             append_by_key(_testing_ids_by_date, key, id)
 
-_average_testing_likes_by_date = {}
-_average_non_testing_likes_by_date = {}
 
-# for key in _testing_ids_by_date:
-#     ids_temp = _testing_ids_by_date.get(key)
-#     unique = len(set(ids_temp))
-#     if unique != 0:
-#         _average_testing_likes_by_date[key] = len(ids_temp) / unique
-#     else:
-#         _average_testing_likes_by_date[key] = 0
-get_avarage_likes_by_date(_average_testing_likes_by_date, _testing_ids_by_date)
-
-# for key in _non_testing_ids_by_date:
-#     ids_temp = _non_testing_ids_by_date.get(key)
-#     unique = len(set(ids_temp))
-#     if unique != 0:
-#         _average_non_testing_likes_by_date[key] = len(ids_temp) / unique
-#     else:
-#         _average_non_testing_likes_by_date[key] = 0
-get_avarage_likes_by_date(_average_non_testing_likes_by_date, _non_testing_ids_by_date)
+_average_testing_likes_by_date = get_avarage_likes_by_date(_testing_ids_by_date)
+_average_non_testing_likes_by_date = get_avarage_likes_by_date(_non_testing_ids_by_date)
 
 draw(_average_testing_likes_by_date, _average_non_testing_likes_by_date)
-
